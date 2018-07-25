6f8fa8368dd1ed650f7ba4b4d543efd9035edc71
LabingXEON <LabingXEON@LabingXEON2-PC>
2018-07-25 11:45:24 -0500

    Commit inicial de hola mundo

---
+++ src/Editable/Joption.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Editable;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LabingXEON
+ */
+public class Joption {
+  int numero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un" +
+  " numero para multiplicarlo por 2"));
+}
